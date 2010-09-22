76a3477eff90235e77f458b7e0c69d7878353f5c
Thammi <thammi@chaossource.net>
2010-09-22 16:37:05 +0200

    Adding simple svg wrapper

---
+++ svg.py
@@ -0,0 +1,84 @@
+class Element:
+
+    def __init__(self, tag, paras={}):
+        self.tag = tag
+        self.paras = paras
+
+    def write(self, out):
+        out.write("<{0}".format(self.tag))
+        
+        for key, value in self.paras.iteritems():
+            out.write(' {key}="{value}"'.format(key=key, value=value))
+
+        if hasattr(self, 'payload'):
+            out.write(">\n")
+            self.payload(out)
+            out.write("</{0}>\n".format(self.tag))
+        else:
+            out.write(" />\n")
+
+class Text(Element):
+    def __init__(self, text, position):
+        self.text = text
+
+        paras = {
+                'x': position[0],
+                'y': position[1],
+                }
+
+        Element.__init__(self, 'text', paras)
+
+    def payload(self, out):
+        out.write(self.text)
+
+class Line(Element):
+
+    def __init__(self, (start), (to)):
+        paras = {
+                'x1': start[0],
+                'y1': start[1],
+                'x2': to[0],
+                'y2': to[1],
+                }
+
+        Element.__init__(self, 'line', paras)
+
+class Group(Element):
+
+    def __init__(self):
+        Element.__init__(self, "g")
+        self.subs = []
+
+    def add(self, element):
+        self.subs.append(element)
+
+    def payload(self, out):
+        for sub in self.subs:
+            sub.write(out)
+
+class SVG(Group):
+
+    def __init__(self, size):
+        self.size = size
+
+        Group.__init__(self)
+
+    def write(self, out):
+        # header
+        out.write('<!DOCTYPE svg PUBLIC "-//W3C//DTD SVG 1.0//EN" "http://www.w3.org/TR/2001/REC-SVG-20010904/DTD/svg10.dtd">')
+        out.write('<svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" width="%ipx" height="%ipx">\n' % self.size)
+
+        # background
+        out.write('<rect x="0" y="0" width="%i" height="%i" fill="white" />' % self.size)
+
+        # root group
+        Element.write(self, out)
+
+        # footer
+        out.write('</svg>')
+
+import sys
+svg = SVG((400, 400))
+svg.add(Text("blub", (10, 10)))
+svg.write(sys.stdout)
+
