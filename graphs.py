c03b13a2fd97a6433e674a9003815506808495b2
Thammi <thammi@chaossource.net>
2010-09-22 17:32:27 +0200

    graph: Cleaning up with translation and some documentation

--- graphs.py
+++ graphs.py
@@ -6,6 +6,8 @@ from projects import *
 import svg
 
 def aggre_count(commits, key):
+    """Counting the occurences of aspects defined by a key"""
+
     counter = {}
     
     for commit in commits:
@@ -18,6 +20,8 @@ def aggre_count(commits, key):
     return counter
 
 def plot(data):
+    """Turning [(key, value), ...] into line graphs"""
+
     g = Gnuplot.Gnuplot()
     g('set style data lines')
     #g('set style data linespoints')
@@ -27,68 +31,73 @@ def plot(data):
     raw_input()
 
 def punch_svg(data, out, size = (800, 300)):
-    max_point = max(value for key, value in data)
-    x_amount = max(x for (x, y), value in data) + 2
-    y_amount = max(y for (x, y), value in data) + 2
+    """Turning [((x, y), value), ...] into punchcards"""
 
-    x_step = float(size[0]) / x_amount
-    y_step = float(size[1]) / y_amount
+    max_point = max(value for key, value in data)
+    x_amount = max(x for (x, y), value in data) + 1
+    y_amount = max(y for (x, y), value in data) + 1
+
+    x_step = float(size[0]) / (x_amount + 1)
+    y_step = float(size[1]) / (y_amount + 1)
     box_size = min(float(size[0])/x_amount, float(size[1])/y_amount)
     size_step = box_size / 2 * 0.8 / max_point
     font_size = box_size*0.7
     
+    # root of the svg image
     root = svg.SVG(size)
     root.style('fill', 'black')
     root.style('stroke', 'grey')
 
+    # group transformed into the area containing the data
+    card = svg.Group()
+    root.add(card)
+    card.translate((x_step, y_step))
+
+    # group containing the captions
     caption = svg.Group()
-    root.add(caption)
+    card.add(caption)
     caption.style('fill', 'grey')
     caption.style('font-size', font_size)
     caption.style('text-anchor', 'middle')
 
     # vertical captions
-    for step in range(y_amount - 1):
-        y = y_step * (step + 2) - (box_size - font_size) / 2
-        x = x_step / 2
+    for step in range(y_amount):
+        y = y_step * (step + 1) - (box_size - font_size) / 2
+        x = - x_step / 2
 
         caption.add(svg.Text(str(step), (x, y)))
 
     # horizontal captions
-    for step in range(x_amount - 1):
-        x = x_step * (step + 1) + x_step / 2
-        y = y_step - (box_size - font_size) / 2
+    for step in range(x_amount):
+        x = x_step * step + x_step / 2
+        y = -(box_size - font_size) / 2
 
         caption.add(svg.Text(str(step), (x, y)))
 
     # painting horizontal lines
-    for step in range(y_amount + 1):
+    for step in range(y_amount + 2):
         y = y_step * step
         root.add(svg.Line((0, y), (size[0], y)))
 
     # painting vertical lines
-    for step in range(x_amount + 1):
+    for step in range(x_amount + 2):
         x = x_step * step
         root.add(svg.Line((x, 0), (x, size[1])))
 
     # painting the data
     for (x, y), value in data:
-        x = x_step * (x + 1) + x_step / 2
-        y = y_step * (y + 1) + y_step / 2
+        x = x_step * x + x_step / 2
+        y = y_step * y + y_step / 2
         radius = size_step * value
 
-        root.add(svg.Circle(radius, (x, y)))
+        card.add(svg.Circle(radius, (x, y)))
 
     root.write(out)
 
 def main(argv):
     b = Base()
 
-    #data = b.commits()
-    #data = b.find_user("s2960869") # me
-    data = b.find_user("s8572327") # twobit
-    #data = b.find_user("s3957022") # kilian
-    #data = b.find_user("s1394474") # payload
+    data = b.commits()
     #data = b.groups[25].commits()
 
     agg = aggre_count(data, lambda c: (c['date'][3], date_to_weekday(c['date'])))
