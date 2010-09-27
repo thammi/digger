0a8be12d524eda4d600e2acbf1c04cc09f344173
Jens Abraham <Jens_Abraham@gmx.de>
2010-09-27 22:21:43 +0200

    log_to_json: working online/offline counter

--- log_to_json.py
+++ log_to_json.py
@@ -13,17 +13,18 @@ DATEREG = "[\d]{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2}"
 f = open("./core.log", "r")
 g = open("./raw_log.json", "w")
 
-i = 0
-
+online_counter = 0
+offline_counter = 0
 for s in f:
     s = s.strip("\n")
     if s.find("core") != -1:
         timeblob = re.match(DATEREG, s)
-        if s.find("+v") or s.find("has joined"):
+        if s.find("+v") != -1 or s.find("has joined") != -1:
             json.dump({'nick' : NICK, 'time' : timeblob.group(0), 'action' : 'online'}, g)
-        elif s.find("-v") or s.find("has quit"):
+            online_counter += 1
+        elif s.find("-v") != -1 or s.find("quit") != -1:
             json.dump({'nick' : NICK, 'time' : timeblob.group(0), 'action' : 'offline'}, g)
-        i += 1
+            offline_counter += 1
 f.close
 
-print "%i entrys written" % i
+print "%i online entrys and %i offline entrys found" % (online_counter, offline_counter)
