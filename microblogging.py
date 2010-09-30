82f4aee7d614a0300ad0ad88325b7453ce892c5c
Jens Abraham <Jens_Abraham@gmx.de>
2010-09-30 20:55:43 +0200

    little fix for twitter-api

--- microblogging.py
+++ microblogging.py
@@ -7,9 +7,28 @@ from warnings import warn
 
 from json_batch import save_batch
 
-def get_updates(service, user, count = 200, page = 1):
+def get_updates(service, user, count = 200, page = 1, updatescount = -1):
     if service in ["identica", "twitter"]:
-        print "Fetching page", page
+        print "Fetching page %i, %i updates remaining" % (page, updatescount)
+
+        base_url = { 'identica' : "http://identi.ca/api/",
+                     'twitter' : "http://api.twitter.com/1/",
+                     }
+
+        if updatescount == -1:
+
+            query = urllib.urlencode({
+                    'id': user
+                    })
+
+            res = urllib.urlopen("%susers/show.json?%s" % (base_url[service], query))
+
+            userdata = json.load(res)
+
+            updatescount = userdata['statuses_count']
+
+        if updatescount < 200:
+            count = updatescount
 
         query = urllib.urlencode({
                 'page': page,
@@ -17,22 +36,20 @@ def get_updates(service, user, count = 200, page = 1):
                 'id': user
                 })
 
-        base_url = { 'identica' : "http://identi.ca/api/statuses/user_timeline.json",
-                     'twitter' : "http://api.twitter.com/1/statuses/user_timeline.json",
-                     }
-
-        res = urllib.urlopen("%s?%s" % (base_url[service], query))
+        res = urllib.urlopen("%sstatuses/user_timeline.json?%s" % (base_url[service], query))
 
         if res.getcode() < 300:
             updates = json.load(res)
-
+            
             if len(updates) == count:
-                updates.extend(get_updates(service, user, count, page + 1))
-
+                if (updatescount - count > 0):
+                    updates.extend(get_updates(service, user, count, page + 1, updatescount - count))
+                
                 return updates
             else:
                 print "Unable to fetch: %i '%s'" % (res.getcode(), res.info())
                 return None
+
     else:
         print "please specify correct service (identica or twitter)"
         return None
@@ -53,7 +70,7 @@ if __name__ == '__main__':
             if not updates:
                 print "ERROR: No results!"
             else:
-                save_batch(user, dents, "raw_updates_%s.json" % service)
+                save_batch(user, updates, "raw_updates_%s.json" % service)
 
                 print "Amount of updates:  %i" % len(updates)
                 print
