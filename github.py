24e063523bfaf9132d4db5e10ddae73d379c9a80
Thammi <thammi@chaossource.net>
2010-10-06 05:35:28 +0200

    github: fetching whole users

--- github.py
+++ github.py
@@ -6,7 +6,7 @@ import re
 import urllib
 from warnings import warn
 
-from json_batch import save_batch
+from json_batch import save_batch, update_batch
 
 class ServiceFailedException(Exception):
 
@@ -16,17 +16,17 @@ class ServiceFailedException(Exception):
     def __str__(self):
         return self.msg
 
-def api_call(method, repo, options={}):
+def api_call(method, params, query={}):
     base_url = "http://github.com/api/v2/json/"
          
     url_parts = {
-        'query' : urllib.urlencode(options),
+        'query' : urllib.urlencode(query),
         'base_url': base_url,
         'method': method,
-        'repo' : repo
+        'params' : '/'.join(params),
         }
 
-    res = urllib.urlopen("{base_url}{method}/{repo}?{query}".format(**url_parts))
+    res = urllib.urlopen("{base_url}{method}/{params}?{query}".format(**url_parts))
 
     if res.getcode() < 300:
         return json.load(res)
@@ -40,7 +40,7 @@ def fetch_repo(user, repo, branch='master'):
     page = 1
     commits = []
 
-    repo_id = '/'.join((user, repo, branch))
+    repo_id = (user, repo, branch)
 
     # how many dents are there?
     #count = api_call('users/show', {'id': user})['statuses_count']
@@ -65,8 +65,23 @@ def fetch_repo(user, repo, branch='master'):
 
     return commits
 
-def fetch_user():
-    raise NotImplementedError
+def fetch_user(user):
+    repos = api_call("repos/show", [user])
+    batch = {}
+
+    for repo in repos['repositories']:
+        # TODO: fetch all branches or decide which to fetch
+        name = repo['name']
+        repo_id = '/'.join((user, name, 'master'))
+
+        print "==> Fetching %s" % name
+
+        try:
+            batch[repo_id] = fetch_repo(user, name, 'master')
+        except ServiceFailedException as ex:
+            print "Failed in %s: %s" % (name, str(ex))
+
+    return batch
 
 if __name__ == '__main__':
     batch_fn = "raw_github_commits.json"
@@ -90,6 +105,7 @@ if __name__ == '__main__':
         if not commits:
             print "ERROR: No results!"
         else:
+            repo_id = '/',join((user, repo, branch))
             save_batch(repo, commits, batch_fn)
 
             print "Amount of commits:  %i" % len(commits)
